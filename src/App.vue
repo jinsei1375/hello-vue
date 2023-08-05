9c8095dc42acd1447ab7c16e029815e86e4e586c
murakami <murakami@kokochie.jp>
2023-08-05 22:26:32 +0900

    リアクティブデータの取り扱い

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup lang="ts">
+import { ref, computed, reactive } from 'vue'
+
+const name = ref("田中太郎");
+
+const now = new Date();
+const nowStr = now.toLocaleTimeString();
+
+let timeStr = nowStr; // リアクティブでない変数として宣言
+
+const timeStrRef = ref(nowStr);
+
+function changeTime(): void {
+  const newTime = new Date();
+  const newTimeStr = newTime.toLocaleTimeString();
+  timeStrRef.value = newTimeStr;
+}
+
+setInterval(changeTime, 1000);
+
+
+// const radiusInt = Math.round(Math.random() * 10);
+// const PI = ref(3.14);
+// const radius = ref(radiusInt);
+// ref()とreactive()の違い
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10),
+})
+const area = computed((): number => {
+  return data.radius * data.radius * data.PI;
+});
+
+setInterval(
+  ():void => {
+    data.radius = Math.round(Math.random() * 10);
+  },
+  5000
+);
+console.log(data)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>こんにちは！{{ name }}さん</h1><br>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <p>現在時刻：{{ timeStr }}</p><br>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <p>現在時刻(ref)：{{ timeStrRef }}</p><br>
+
+  <p>半径{{ data.radius }}の円の半径を円周率{{ data.PI }}で計算すると、{{ area }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
